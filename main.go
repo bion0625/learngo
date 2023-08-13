6f2f0b18a8291c9e83fd2d736f9ea4a36308acfa
이의정 <stxtory@gmail.com>
2023-08-13 16:28:52 +0900

    url checker

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errorRequestFailed = errors.New("Request Failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.co.kr/",
 		"https://www.google.com/",
@@ -19,9 +20,16 @@ func main() {
 		"https://www.instagram.com/",
 		"https://nomadcoders.co/",
 	}
-
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -29,6 +37,7 @@ func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errorRequestFailed
 	}
 	return nil
