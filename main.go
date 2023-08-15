736f1f3bb92cac50d8a85789bdfa3ded07f2101a
이의정 <stxtory@gmail.com>
2023-08-15 22:42:44 +0900

    fast url checker

--- main.go
+++ main.go
@@ -1,68 +1,49 @@
-// package main
-
-// import (
-// 	"errors"
-// 	"fmt"
-// 	"net/http"
-// )
-
-// var errorRequestFailed = errors.New("Request Failed")
-
-// func main() {
-// 	var results = make(map[string]string)
-// 	urls := []string{
-// 		"https://www.airbnb.co.kr/",
-// 		"https://www.google.com/",
-// 		"https://www.amazon.com/",
-// 		"https://www.reddit.com/",
-// 		"https://soundcloud.com/",
-// 		"https://www.facebook.com/",
-// 		"https://www.instagram.com/",
-// 		"https://nomadcoders.co/",
-// 	}
-// 	for _, url := range urls {
-// 		result := "OK"
-// 		err := hitURL(url)
-// 		if err != nil {
-// 			result = "FAILED"
-// 		}
-// 		results[url] = result
-// 	}
-// 	for url, result := range results {
-// 		fmt.Println(url, result)
-// 	}
-// }
-
-// func hitURL(url string) error {
-// 	fmt.Println("Checking:", url)
-// 	resp, err := http.Get(url)
-// 	if err != nil || resp.StatusCode >= 400 {
-// 		fmt.Println(err, resp.StatusCode)
-// 		return errorRequestFailed
-// 	}
-// 	return nil
-// }
-
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type requestResult struct{
+	url string
+	status string
+}
+
+var errorRequestFailed = errors.New("Request Failed")
+
 func main() {
-	c := make(chan string)
-	people := [4]string{"UJ", "flynn","a","b"}
-	for _, person := range people {
-		go isSexy(person, c)
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.co.kr/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://nomadcoders.co/",
 	}
-	fmt.Println("Waiting for message")
-	for i:=0;i<len(people);i++{
-		fmt.Println(<-c)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i:=0;i<len(urls);i++{
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + "is sexy"
+func hitURL(url string, c chan requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		status = "FAILED"
+	}
+	c <- requestResult{url:url, status: status}
 }
